26a3614ceac9bdb0ae0c64235ad659f3976c00d6
shashi-06-03 <109144797+shashi-06-03@users.noreply.github.com>
2022-07-28 11:06:42 +0530

    Add files via upload

---
+++ 6. Write a program to print the first 10 even natural numbers.c
@@ -0,0 +1,9 @@
+//6. Write a program to print the first 10 even natural numbers
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=2;i<=20;i+=2)
+        printf("%d\n",i);
+     return 0;
+}
---
+++ 7. Write a program to print the first 10 even natural numbers in reverse order.c
@@ -0,0 +1,9 @@
+//7. Write a program to print the first 10 even natural numbers in reverse order
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=20;i>=2;i-=2)
+        printf("%d\n",i);
+     return 0;
+}
